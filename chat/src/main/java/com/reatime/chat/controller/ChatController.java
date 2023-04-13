706f426e0a47a560548185f300a5bbc2314da3ff
Nguyễn Minh <91356207+nguyenxvotanminh3@users.noreply.github.com>
2023-04-13 14:30:04 +0700

    không có gì

--- chat/src/main/java/com/reatime/chat/controller/ChatController.java
+++ chat/src/main/java/com/reatime/chat/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-
+// anotate ngu ngu
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
